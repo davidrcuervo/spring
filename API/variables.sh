2341f8bfb34188940b900eb73ae3fc03f84d97fa
david <davidrcuervo@gmail.com>
2024-06-13 21:50:28 -0400

    Upgrade pom files to support Spring boog 3.2.6

--- API/variables.sh
+++ API/variables.sh
@@ -2,10 +2,9 @@
 
 # Author : MySelf.1664
 # Create variables
-
 echo "Loading local variables"
 
-SERVER_ADDRESS="127.0.0.1"
+SERVER_ADDRESS="172.19.208.1"
 USERNAME="admuser"
 PASSWORD="secret"
 MAX_TIME=10
