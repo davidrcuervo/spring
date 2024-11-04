12d3d705e0b4b12ff9391a34102bc073bfb639b1
i849921 <davidrcuervo@gmail.com>
2024-11-04 00:23:52 -0500

    docker start files correction 1

--- messenger/start.sh
+++ messenger/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 JASYPT_PASSWORD=$(cat /run/secrets/jasypt-password)
+DIR=$(pwd)
 mvn spring-boot:run \
 "-Dspring-boot.run.jvmArguments=-Djasypt.encryptor.password=$JASYPT_PASSWORD \
 -Dspring.config.additional-location=$DIR/spring/API/,$DIR/spring/" \
