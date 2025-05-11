74c28172c533c2fea6fdf26e67660071627a5ce9
david <davidrcuervo@gmail.com>
2025-05-11 16:37:21 -0400

    userKc start.sh requires to be updated

--- userKc/start.sh
+++ userKc/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 JASYPT_PASSWORD=$(cat /run/secrets/jasypt-password)
 DIR=$(pwd)
+
 mvn spring-boot:run \
 "-Dspring-boot.run.jvmArguments=-Djasypt.encryptor.password=$JASYPT_PASSWORD \
 -Dspring.config.additional-location=$DIR/spring/API/,$DIR/spring/" \
--f spring/usuaKc/pom.xml
+-f spring/userKc/pom.xml
