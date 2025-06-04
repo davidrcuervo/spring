dc72593097dfb672b2d2831fbdb6d52dddc857a2
I849921 <davidrcuervo@gmail.com>
2025-06-04 17:16:26 -0400

    nginx entrypoint modified to wait frontend to start before enabling reverse proxing for it

--- docker/scripts/nginx/nginx.entrypoint.sh
+++ docker/scripts/nginx/nginx.entrypoint.sh
@@ -1,5 +1,37 @@
 #!/usr/bin/env bash
 
+function close(){
+  echo "Closing nginx..."
+  rm /etc/nginx/sites-enabled/keycloak
+  rm /etc/nginx/sites-enabled/webapp
+  echo "...Bye nginx"
+  exit 0
+}
+
+function start_webapp(){
+  COUNTER=0
+  WEBAPP_FLAG=false
+
+  while [ $COUNTER -lt 10 ]
+  do
+    if nc -z frontend "$PORT_FRONTEND"; then
+      echo "Starting webapp"
+      ln -s /etc/nginx/sites-available/webapp.nginx /etc/nginx/sites-enabled/webapp
+      nginx -s reload
+      WEBAPP_FLAG=true
+      break
+    else
+      sleep 2
+      COUNTER=$((COUNTER+1))
+      echo "waiting webapp to start..."
+    fi
+  done
+
+  if [ "$WEBAPP_FLAG" = false ]; then
+    echo "Nginx failed to start webapp frontend."
+  fi
+}
+
 function run(){
   export DOLLAR="$"
 
@@ -7,10 +39,14 @@ function run(){
   envsubst < keycloak.template.nginx > /etc/nginx/sites-available/keycloak.nginx
   envsubst < webapp.template.nginx > /etc/nginx/sites-available/webapp.nginx
   ln -s /etc/nginx/sites-available/keycloak.nginx /etc/nginx/sites-enabled/keycloak
-  ln -s /etc/nginx/sites-available/webapp.nginx /etc/nginx/sites-enabled/webapp
+#  ln -s /etc/nginx/sites-available/webapp.nginx /etc/nginx/sites-enabled/webapp
 
+  trap close SIGTERM SIGINT SIGQUIT SIGHUP
   echo "Starting nginx service"
-  nginx -g "daemon off;"
+  nginx -g "daemon off;" &
+  NGINX_PID=$!
+  start_webapp
+  wait "$NGINX_PID"
 }
 
 if [ ! -f keycloak.template.nginx ];then
