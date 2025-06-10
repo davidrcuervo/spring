1f88ec66f6b8aa8745d8ed521aa506cada3b3c25
myself <davidrcuervo@gmail.com>
2025-06-10 15:02:04 -0400

    Remove usuarioLdap project/folder

--- usuarioLdap/src/main/java/com/laetienda/usuario/repository/MistakeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.laetienda.usuario.repository;
-
-import com.laetienda.lib.model.Mistake;
-
-public interface MistakeRepository {
-
-    public void addMistake(String name, String message);
-    public Mistake get();
-}
--- usuarioLdap/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-JASYPT_PASSWORD=$(cat /run/secrets/jasypt-password)
-DIR=$(pwd)
-mvn test \
--Djasypt.encryptor.password=$JASYPT_PASSWORD \
--Dspring.config.additional-location=$DIR/spring/API/,$DIR/spring/ \
--f spring/usuario/pom.xml
